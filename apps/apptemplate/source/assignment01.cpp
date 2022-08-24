e8c338ffb0c4d180befe890075e773c1ddf17f9c
jasoncassimus <45297466+jasoncassimus@users.noreply.github.com>
2022-08-24 13:37:02 -0500

    Update assignment01.cpp
    Here are just a few pointer errors, and some explanation of what happens.

--- apps/apptemplate/source/assignment01.cpp
+++ apps/apptemplate/source/assignment01.cpp
@@ -1,15 +1,36 @@
 //
 // File:   assignment1.cpp
-// Author: <PUT YOUR NAME HERE>
+// Author: Jason Cassimus
 // Purpose:
 // Illustrate some of the bad thins that can happen with
 // pointers
-//
+
+//https://www.acodersjourney.com/top-20-c-pointer-mistakes/
+
 #include <iostream>
 using namespace std;
 
 
 int main() {
-  // Put your code here.
+  int* p1, p2; //one is a pointer to an int, and the other is an int
+  int n
+  n = *p1  // Error: The pointer is unitialized
+  //------------------------------------------------------------------
+  p1 = &n  // Error: Pointing to uninitialized variable
+  //------------------------------------------------------------------
+  n = 30;
+  p1 = &n;
+  p2 = &n; // “Error C2440 ‘=’: cannot convert from ‘int *’ to ‘int’ ”
+  //------------------------------------------------------------------
+  // Determine size of an array
+  const char arr[] = "hello";
+  const char *cp = arr;
+  printf("Size of arr %lu\n", (int)sizeof(arr));
+  printf("Size of *cp %lu\n", (int)sizeof(*cp));  // Error size = 1
+  
+  
+  
   return 0;
+  
+  
 }
